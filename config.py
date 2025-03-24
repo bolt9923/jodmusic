019f560093975d446eeec0353574e24618c5238f
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-03-24 15:42:17 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/bolt9923/jodmusic",
+    "https://t.me/the_friends_junction",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
