c65a42e98072c952cf8d6092ff44e5c651edac18
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-03-24 15:02:03 +0530

    Update autoleave.py

--- AviaxMusic/plugins/misc/autoleave.py
+++ AviaxMusic/plugins/misc/autoleave.py
@@ -27,7 +27,7 @@ async def auto_leave():
                     ]:
                         if (
                             i.chat.id != config.LOG_GROUP_ID
-                            and i.chat.id != -1002016928980 and i.chat.id != -1002200386150 and i.chat.id != -1001397779415
+                            and i.chat.id != -1002653658491 and i.chat.id != -1002200386150 and i.chat.id != -1001397779415
                         ):
                             if left == 20:
                                 continue
